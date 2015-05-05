be3fa66aad6020d662b73780e79996b79be1689c
ohad <ohadgur@gmail.com>
2015-05-05 17:35:36 +0300

    Added: added requests handlers

--- src/main/java/RequestsModule/RequestsHandler.java
+++ src/main/java/RequestsModule/RequestsHandler.java
@@ -1,5 +1,7 @@
 package RequestsModule;
 
+import RequestsModule.utils.HashMapCreator;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +25,6 @@ public class RequestsHandler {
     private final String RESEND_EMAIL = "resendEmail";
 
     /*** Routine Requests Codes ***/
-    private final String VERIFYDOCTOR = "verifyDoctor";
     private final String ASKWAITINGPATIENTS = "askWaitingPatients";
     private final String CURRENTLOCATION = "currentLocation";
 
@@ -79,7 +80,11 @@ public class RequestsHandler {
                 case SIGNIN:
                     rc.signIn(requestMap);
                     break;
-
+                case ASKWAITINGPATIENTS:
+                    JSONArray respone  = (JSONArray) rc.getWaitingForDoctor(Integer.parseInt(requestMap.get("cmid")));
+                    if (respone != null){
+                        return respone.toString();
+                    }
                 default:
                     // Do nothing...
                     break;
@@ -88,5 +93,20 @@ public class RequestsHandler {
             ex.printStackTrace();
         }
         return (new JSONObject().put("RequestAccepted", reqId).toString());
+    }
+
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.HEAD}, value = "/verify_email")
+    public @ResponseBody String handleEmailVerification(@RequestParam String key /*CMID right now*/){
+        String reqId = "";
+
+        try {
+            IRegController rc = new RegController_V1();
+            rc.verifyDetail(null); // TODO - insert the key
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return "Email Verified";
     }
 }
--- src/main/java/RequestsModule/utils/HashMapCreator.java
+++ src/main/java/RequestsModule/utils/HashMapCreator.java
@@ -1,4 +1,4 @@
-package RequestsModule;
+package RequestsModule.utils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
