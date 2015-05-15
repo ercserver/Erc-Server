cdea9ecd98969d5c0beee97322a02626e45b14f9
מאור <maorsegal3@gmail.com>
2015-05-15 23:19:19 +0300

    ...

--- src/test/java/registratinModule/MaorTests.java
+++ src/test/java/registratinModule/MaorTests.java
@@ -2,6 +2,7 @@ package registratinModule;
 
 import DatabaseModule.src.model.DbComm_V1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -16,13 +17,40 @@ public class MaorTests {
         System.out.println(h1);
     }
 
-   /* public Object updatedFromPatient(HashMap<String, String> filledForm)
+    /*public Object updatedFromPatient(HashMap<String, String> filledForm)
     {
-        if checkCmidAndPassword(filledForm.get("password"), Integer.parseInt(filledForm.get("community_member_id"))
+        if (!checkCmidAndPassword(filledForm.get("password"), Integer.parseInt(filledForm.get("community_member_id"))))
         {
-
+            return null;
         }
-        else
+        String eventId = dbController.getEventByCmid(filledForm.get("community_member_id"));
+        HashMap<String, String> response = new HashMap<String, String>();
+        response.put("event_id", eventId);
+        response.put("message", filledForm.get("message"));
+        response.put("RequestID", "newInfo");
+        ArrayList<String> regIds = dbController.getHelpersRegIds(eventId);
+        return popUpMessage(response, regIds, true);
+    }
 
+    private Object popUpMessage(HashMap<String, String> response, ArrayList<String> regIds, boolean sendToEms)
+    {
+        if(sendToEms)
+        {
+            if(null != regIds)
+            {
+                commController.setCommToUsers(response, null, false);
+                commController.sendResponse();
+                commController.setCommToUsers(response, regIds, false);
+                return commController.sendResponse();
+            }
+            commController.setCommToUsers(response, null, false);
+            return commController.sendResponse();
+        }
+        if(null != regIds)
+        {
+            commController.setCommToUsers(response, regIds, false);
+            return commController.sendResponse();
+        }
+        return null;
     }*/
 }
