797fd8b188d035b2bd80b41363b6a531e6ad944d
מאור <maorsegal3@gmail.com>
2015-06-05 15:37:57 +0300

    ...

--- src/main/java/EmergencyModule/src/model/EmerLogger_V1.java
+++ src/main/java/EmergencyModule/src/model/EmerLogger_V1.java
@@ -1,6 +1,8 @@
 package EmergencyModule.src.model;
 
+import DatabaseModule.src.api.IDbController;
 import EmergencyModule.src.api.IEmerLogger_model;
+import Utilities.ModelsFactory;
 
 import java.util.HashMap;
 
@@ -8,6 +10,15 @@ import java.util.HashMap;
  * Created by NAOR on 23/05/2015.
  */
 public class EmerLogger_V1 implements IEmerLogger_model {
+
+    private IDbController dbController = null;
+
+    public EmerLogger_V1()
+    {
+        ModelsFactory models = new ModelsFactory();
+        dbController = models.determineDbControllerVersion();
+    }
+
     @Override
     public void handleArrivalToDest(HashMap<String, String> data) {
         //TODO - Michael
@@ -26,5 +37,25 @@ public class EmerLogger_V1 implements IEmerLogger_model {
     @Override
     public void handleUpdatePatientStatus(HashMap<String, String> data) {
         //TODO - Michael
+    }
+
+    @Override
+    public void handleHelpCalling(String eventId) {
+        dbController.updateLogs(eventId, "'Patient called for help'");
+    }
+
+    @Override
+    public void handleGettingCall(String eventId) {
+        dbController.updateLogs(eventId, "'Server sended to patient's app call getting'");
+    }
+
+    @Override
+    public void handleSearchingClosestEMS(String eventId) {
+        dbController.updateLogs(eventId, "'Server asked GIS for the closest EMS'");
+    }
+
+    @Override
+    public void handleGettingCmidOfEMS(String event_id) {
+        dbController.updateLogs(event_id, "'EMS sended to the server his CMID'");
     }
 }
