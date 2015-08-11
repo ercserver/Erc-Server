3ed5c4d4e3816e2b996dff596e1d87559e6b9d7c
מאור <maorsegal3@gmail.com>
2015-08-11 16:13:05 +0300

    ...

--- src/test/java/registratinModule/ShmulikTest.java
+++ src/test/java/registratinModule/ShmulikTest.java
@@ -19,8 +19,8 @@ import java.util.logging.Level;
 public class ShmulikTest {
     public static void main(String[] args) {
         //testNaor3("10005");
-        //testNaor4("10005");
-        testNaor5();
+        testNaor4("10005");
+       // testNaor5();
        /* DbComm_V1 d = new DbComm_V1();
         testNaor1();
 
